861deb36360fb5b7c29cebb5db2d4961fb503cf7
Lallethu <Lallethu.68@gmail.com>
2024-10-20 02:21:37 +0200

    Création d'une structure de donnée abstraite pour les unités.

---
+++ common/data_resources/unit_data.gd
@@ -0,0 +1,13 @@
+class_name UnitData
+extends Resource
+
+@export_category("Unit settings")
+@export var name: String
+@export var speed: float = 1.0
+@export var max_health: int = 100
+@export var attack_power: int = 10
+@export var attack_range: float = 1.5
+@export var experience_max: float = 1.75
+@export var resources_cost: Dictionary
+@export var creation_time: int
+@export var is_worker: bool = false
